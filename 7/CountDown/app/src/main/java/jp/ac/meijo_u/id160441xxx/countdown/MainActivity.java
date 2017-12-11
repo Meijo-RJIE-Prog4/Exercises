7e98badd2638774571eb2efc7c47dfcb234fb9a5
Hidekazu Suzuki <hsuzuki@ucl.meijo-u.ac.jp>
2017-12-11 20:47:54 +0900

    第7回演習で使用するAndroidサンプルアプリケーション

---
+++ 7/CountDown/app/src/main/java/jp/ac/meijo_u/id160441xxx/countdown/MainActivity.java
@@ -0,0 +1,79 @@
+package jp.ac.meijo_u.id160441xxx.countdown;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.*;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editCount;
+    private Button buttonStart;
+    private Button buttonStop;
+    private TextView textCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        editCount = (EditText)findViewById(R.id.editCount);
+        buttonStart = (Button)findViewById(R.id.buttonStart);
+        buttonStop = (Button)findViewById(R.id.buttonStop);
+        textCount = (TextView)findViewById(R.id.textCount);
+    }
+
+    public void handleButtonStart(View view) {
+        int count = Integer.parseInt(editCount.getText().toString());
+        for (int i = count; i >= 0; i--) {
+            textCount.setText(String.valueOf(i));
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        textCount.setText("Done!");
+    }
+
+    public void handleButtonStop(View view) {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
