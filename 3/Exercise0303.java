120f3a72b79490b1f22abea3134a10b19573417f
Hidekazu Suzuki <hsuzuki@meijo-u.ac.jp>
2018-10-16 10:49:01 +0900

    演習0303の雛型を追加

---
+++ 3/Exercise0303.java
@@ -0,0 +1,29 @@
+/**
+ * 演習0303用ソースコード
+ * @author	Hidekazu Suzuki
+ * @date	2018/10/16
+ * @version	1.0
+ */
+public class Exercise0303 {
+
+	public static void main(String[] args) {
+		// 炎の剣を作成（ダメージ=10）
+		Sword sword = new Sword();
+		
+		// 勇者ミナトの作成（HP=100）
+		Hero h = new Hero();
+		
+		// 勇者の攻撃
+		h.attack();
+		
+		// 所持金を100に設定
+
+		// お化けキノコA~Eを生成し，配列で管理
+
+		// お化けキノコが順番に逃げる
+
+		// 所持金を表示
+
+	}
+
+}
